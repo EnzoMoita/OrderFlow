342f0a58cbe02037587ba2e9e320a52afecb23f8
Enzo Salomão <enzosadourado@hotmail.com>
2024-05-08 12:42:50 -0300

    filtersandfinishtable

---
+++ src/pages/auth/app/orders/order-table-row.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button"
+import { TableRow, TableCell } from "@/components/ui/table"
+import { ArrowRight, X, Search} from "lucide-react"
+
+
+// export interface OrderTableRowProps {}
+
+export function OrderTableRow() { 
+return(
+    <TableRow>
+    <TableCell>
+        <Button variant="outline" size="xs">
+            <Search className="h-3 w-3" />
+            <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+    </TableCell>
+    <TableCell className="font-mono text-xs font-medium">
+        er2rr2r2r
+        </TableCell>
+    <TableCell className="text-muted-foreground">
+         Há 15 minutos
+    </TableCell>
+    <TableCell>
+        <div className="flex items-center gap-2">
+        <span className="h-2 w-2 rounded-full bg-slate-400"/>
+        <span className="font-medium text-muted-foreground">
+            Pendente
+            </span>
+        </div>
+    </TableCell>
+    <TableCell className="font-medium">
+        Enzo Salomão Dourado
+    </TableCell>
+    <TableCell className="font-medium">R$149,00</TableCell>
+    <TableCell>
+    <Button variant="outline" size="xs">
+            <ArrowRight className="h-3 w-3 mr-2" />
+            Aprovar
+        </Button>
+    </TableCell>
+    <TableCell>
+    <Button variant="ghost" size="xs">
+            <X className="h-3 w-3 mr-2" />
+            Cancelar
+        </Button>
+    </TableCell>
+    </TableRow>
+)
+
+}
