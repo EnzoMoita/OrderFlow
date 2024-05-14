fbdb9233b8dad0c5292af1f64deec6f8d1e189f1
Enzo Salomão <enzosadourado@hotmail.com>
2024-05-14 13:19:52 -0300

    addapiclient

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './global.css'
 import { router } from './routes'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -11,7 +13,9 @@ export function App() {
       <ThemeProvider storageKey="orderflow-theme" defaultTheme="dark">
       <Helmet titleTemplate="%s | OrderFlow" />
       <Toaster richColors/>
+      <QueryClientProvider client={queryClient}>
       <RouterProvider router ={router} />
+      </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
     
