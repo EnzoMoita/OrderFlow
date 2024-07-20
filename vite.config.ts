dcf1b89137cf9cb7103e1945443859efa39f105b
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-19 22:49:05 -0300

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,19 @@ export default defineConfig({
     environment: 'happy-dom',
   },
   build: {
-    sourcemap: false, // Desativa sourcemaps para produção
+    sourcemap: false,  // Desativa sourcemaps para produção
+    rollupOptions: {
+      output: {
+        // Configuração de chunks manual para bibliotecas grandes ou comuns
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            // Retorna o nome do pacote
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Aumenta o limite de aviso de tamanho de chunk para 1000kb
   }
 } as UserConfig & {
   test: InlineConfig
