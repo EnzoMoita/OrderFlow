b367ca319190b7ae2ed5bd154cc81f8032e12967
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-19 23:27:11 -0300

    NewTests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "node:path";
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import type { UserConfig } from "vite";
@@ -11,26 +11,25 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: {
+    include: ['@tanstack/react-query']  // Inclua aqui os módulos que deseja pré-transpilar
+  },
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
     environment: 'happy-dom',
   },
   build: {
-    sourcemap: false,  // Desativa sourcemaps para produção
+    sourcemap: true,
     rollupOptions: {
       output: {
-        // Configuração de chunks manual para bibliotecas grandes ou comuns
         manualChunks: (id) => {
           if (id.includes('node_modules')) {
-            // Retorna o nome do pacote
             return id.toString().split('node_modules/')[1].split('/')[0];
           }
         }
       }
     },
-    chunkSizeWarningLimit: 1000, // Aumenta o limite de aviso de tamanho de chunk para 1000kb
+    chunkSizeWarningLimit: 1000,
   }
-} as UserConfig & {
-  test: InlineConfig
-});
+} as UserConfig & InlineConfig);
